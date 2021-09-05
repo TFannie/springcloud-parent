c9450c5848ee199f5d507e18523fd32d77c7408e
TFannie <863526911@qq.com>
2021-09-06 01:52:47 +0800

    创建eureka客户端

---
+++ springcloud_02eureka_server/src/main/java/org/EurekaClientApplication.java
@@ -0,0 +1,14 @@
+package org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@SpringBootApplication必须定义在包下，只有这样才能定义其可扫描的子包，包可随意定制
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnableEurekaClient.class,args);
+    }
+}
