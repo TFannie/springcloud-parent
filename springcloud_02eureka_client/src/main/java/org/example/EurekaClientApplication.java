603e75c365e2eae65ee75dd6a195933062efcb42
TFannie <863526911@qq.com>
2021-09-06 23:03:18 +0800

    update .gitignore

--- springcloud_02eureka_client/src/main/java/org/example/EurekaClientApplication.java
+++ springcloud_02eureka_client/src/main/java/org/example/EurekaClientApplication.java
@@ -1,4 +1,4 @@
-package org;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //@SpringBootApplication必须定义在包下，只有这样才能定义其可扫描的子包，包可随意定制
 public class EurekaClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EnableEurekaClient.class,args);
+        SpringApplication.run(EurekaClientApplication.class,args);
     }
 }
