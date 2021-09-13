ce5539555f884ced1da5ae99dd85880be4a862c3
huangjieming <19802021926@139.com>
2021-09-13 17:21:17 +0800

    网卡

---
+++ springcloud_03gateway/src/test/java/org/example/Test.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import java.time.ZonedDateTime;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void test(){
+        System.out.println(ZonedDateTime.now());
+    }
+}
